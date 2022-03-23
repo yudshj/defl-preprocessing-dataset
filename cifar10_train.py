9ab833a0023763dfcb5a365db8ed557d30caadb0
Obsido Han <hanyd@pku.edu.cn>
2022-03-23 11:01:39 +0800

    fine-tuned cifar-10

---
+++ cifar10_train.py
@@ -0,0 +1,76 @@
+import tensorflow as tf
+import tensorflow_addons as tfa
+import numpy as np
+
+
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+  # Restrict TensorFlow to only allocate 1GB of memory on the first GPU
+    try:
+        tf.config.experimental.set_virtual_device_configuration(
+            gpus[0],
+            [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=3200)])
+        logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+    except RuntimeError as e:
+        # Virtual devices must be set before GPUs have been initialized
+        print(e)
+
+
+def load_data_x_y(x_path: str, y_path: str, do_label_flip: bool, to_one_hot: bool, normalize: bool = True):
+    x = np.load(x_path)
+    y = np.load(y_path)
+    y_max = np.max(y)
+    y_min = np.min(y)
+
+    if do_label_flip:
+        y = y_max - y + y_min
+    if to_one_hot:
+        y = tf.keras.utils.to_categorical(y, num_classes=y_max - y_min + 1)
+
+    if normalize:
+        x = x.astype(np.float32) / 255.
+
+    return tf.data.Dataset.from_tensor_slices((x, y))
+
+
+
+# tf.keras.backend.clear_session()
+
+
+data_augmentation = tf.keras.Sequential([
+    tf.keras.layers.RandomFlip('horizontal'),
+    tf.keras.layers.RandomTranslation(4/32, 4/32, fill_mode='nearest'),
+])
+
+
+with tf.device('/cpu:0'):
+    ds = load_data_x_y('cifar10_x_train.npy', 'cifar10_y_train.npy', False, True, True).map(lambda img,label: (data_augmentation(img),label), num_parallel_calls=8)
+    ds = ds.shuffle(buffer_size=50000).batch(32).prefetch(tf.data.AUTOTUNE)
+    # ds = ds.repeat()
+    test_ds = load_data_x_y('cifar10_x_test.npy', 'cifar10_y_test.npy', False, True, True)
+    test_ds = test_ds.batch(128).prefetch(tf.data.AUTOTUNE)
+
+
+
+model = tf.keras.models.load_model('./cifar10_init_model.h5')
+model.summary()
+
+
+
+history = model.fit(
+    ds,
+    epochs=80,
+    # steps_per_epoch=800,
+    validation_data=test_ds,
+    validation_freq=1,
+    callbacks=[tf.keras.callbacks.CSVLogger('cifar10_train.log')]
+)
+
+
+
+print(history.history['val_accuracy'])
+
---
+++ sentiment_train.py
@@ -0,0 +1,160 @@
+import os
+import tensorflow as tf
+import tensorflow_addons as tfa
+import numpy as np
+import scipy.sparse as sp
+from keras import Model
+import subprocess
+os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+
+
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    # Restrict TensorFlow to only allocate 1GB of memory on the first GPU
+    try:
+        tf.config.experimental.set_virtual_device_configuration(
+            gpus[0],
+            [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=4096)])
+        logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+    except RuntimeError as e:
+        # Virtual devices must be set before GPUs have been initialized
+        print(e)
+
+
+
+def _load_data_x_y(x_path: str,
+                   y_path: str,
+                   do_label_flip: bool
+                   ) -> tf.data.Dataset:
+    format = x_path.split('.')[-2:]
+    if format[-1] == 'npy':
+        x = np.load(x_path)
+    elif format[-1] == 'npz' and format[-2] == 'csr':
+        x = sp.load_npz(x_path).toarray()
+    else:
+        raise ValueError(f'Unknown format: {format}')
+
+    y = np.load(y_path)
+    y_max = np.max(y)
+    y_min = np.min(y)
+
+    if do_label_flip:
+        y = y_max - y + y_min
+
+    y = y.astype(np.float32) / 4.0
+
+    ret = tf.data.Dataset.from_tensor_slices((x, y))
+
+    return ret
+
+
+
+# init_model_path = './sentiment140_init_model.h5'
+x_train_path = './sentiment140_x_train.csr.npz'
+y_train_path = './sentiment140_y_train.npy'
+x_test_path = './sentiment140_x_test.csr.npz'
+y_test_path = './sentiment140_y_test.npy'
+batch_size = 1024
+shuffle_train = True
+repeat_train = False
+
+_SEQUENCE_LENGTH = 60
+_LSTM_SIZE = 150
+_KEY_DIM = 300
+_WEIGHT_DECAY = 3e-4
+_DROPOUT = 0.6
+
+
+
+embedding_matrix = np.load('./sentiment140_embedding.npy')
+# not-exists word
+
+embedding_matrix = np.pad(embedding_matrix, ((0,0),(0,1)), 'constant', constant_values=0)
+embedding_matrix[-1][-1] = 1
+
+
+
+tf.keras.backend.clear_session()
+
+
+
+inputs = tf.keras.layers.Input(shape=(_SEQUENCE_LENGTH,))
+x = tf.keras.layers.Embedding(embedding_matrix.shape[0],embedding_matrix.shape[1],weights=[embedding_matrix],trainable=False,mask_zero=True)(inputs)
+x = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(_LSTM_SIZE, return_sequences=True))(x)
+# x = tf.keras.layers.LSTM(_LSTM_SIZE, return_sequences=True)(x)
+
+x = tf.keras.layers.MultiHeadAttention(num_heads=4, key_dim=_KEY_DIM, dropout=_DROPOUT)(x, x)
+x = tf.keras.layers.Flatten()(x)
+x = tf.keras.layers.BatchNormalization()(x)
+
+# x = tf.keras.layers.Dense(512, activation='relu')(x)
+# x = tf.keras.layers.Dropout(0.25)(x)
+# x = tf.keras.layers.BatchNormalization()(x)
+
+outputs = tf.keras.layers.Dense(1, activation='sigmoid')(x)
+model = tf.keras.Model(inputs=inputs, outputs=outputs, name='sentiment140_init_model')
+model.summary()
+
+
+
+# model: Model = tf.keras.models.load_model(init_model_path, compile=True)
+# model.summary()
+
+
+
+with tf.device('/cpu:0'):
+    # train_ds = _load_data_x_y(x_train_path, y_train_path, do_label_flip=False)
+    # test_ds = _load_data_x_y(x_test_path, y_test_path, do_label_flip=False)
+    # # val_ds = None
+    # # TODO: validation dataset may NOT be `None`
+
+    all_ds = _load_data_x_y(x_train_path, y_train_path, do_label_flip=False)
+    test_ds = all_ds.take(len(all_ds) // 6)
+    train_ds = all_ds.skip(len(all_ds) // 6)
+
+    steps_per_epoch = (len(train_ds) + batch_size - 1) // batch_size
+
+    if shuffle_train:
+        train_ds = train_ds.shuffle(buffer_size=100000)
+
+    if repeat_train:
+        train_ds = train_ds.repeat()
+
+    train_ds = train_ds.batch(batch_size).prefetch(tf.data.AUTOTUNE)
+    test_ds = test_ds.batch(batch_size).prefetch(tf.data.AUTOTUNE)
+
+
+
+lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
+    initial_learning_rate=1e-3,
+    decay_steps=steps_per_epoch,
+    decay_rate=0.3,
+    staircase=True
+)
+
+
+
+model.compile(
+    loss=tf.keras.losses.BinaryCrossentropy(),
+    optimizer=tfa.optimizers.AdamW(weight_decay=_WEIGHT_DECAY, learning_rate=lr_schedule),
+    # optimizer=tf.keras.optimizers.Adam(lr_schedule),
+    metrics=[tf.keras.metrics.BinaryAccuracy(), tf.keras.metrics.AUC()],
+)
+
+
+
+history = model.fit(
+    train_ds,
+    epochs=30,
+    steps_per_epoch=steps_per_epoch,
+    validation_data=test_ds,
+    validation_freq=1,
+    callbacks=[tf.keras.callbacks.CSVLogger('sentiment140_train.log')]
+)
+
+
+
+
+
